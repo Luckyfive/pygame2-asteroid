69b2d542d498b0658194953772c59815f2c48d4a
luckyfive <andresf212000@gmail.com>
2025-01-20 20:45:36 -0500

    adding main player render

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print("Starting asteroids!")
@@ -11,12 +12,23 @@ def main():
     pygame.init()
     pygame.display.set_caption("Pygame2 Asteroid")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    main_player = Player(x, y)
+
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0,0,0))
+        main_player.draw(screen)
         pygame.display.flip()
+
+        dt = clock.tick() / 1000
 
 if __name__ == "__main__":
     main()
